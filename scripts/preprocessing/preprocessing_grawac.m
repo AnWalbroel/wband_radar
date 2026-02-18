405f2e56558d4e5bcdc2bfe3c0c09c3d1439eebd
AnWalbroel <walbroel.andreas@gmail.com>
2026-02-18 20:26:42 +0100

    add iop4h2o compatibility

--- scripts/preprocessing/preprocessing_grawac.m
+++ scripts/preprocessing/preprocessing_grawac.m
@@ -1,4 +1,4 @@
-function [data, info] = preprocessing_miraca(data, info, ~)
+function [data, info] = preprocessing_grawac(data, info, ~)
 
 %% ################ add metadata
 
@@ -16,12 +16,13 @@ else
 end
 
 % add info on software version to dataset
-
-if datetimeconv(2025,07,02,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2025,09,01,0,0,0) % VAMPIRE-2
+if datetimeconv(2025,01,25,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2025,03,04,0,0,0) % IOP4H2O (at Ny-Alesund)
+    data.radarsw = '5.65';
+elseif datetimeconv(2025,07,02,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2025,09,01,0,0,0) % VAMPIRE-2
     data.radarsw = '5.65';
 
 else    
-    disp('From function processing_miraca: Radar software version not defined!')
+    disp('From function preprocessing_grawac: Radar software version not defined!')
     
 end % if
 
