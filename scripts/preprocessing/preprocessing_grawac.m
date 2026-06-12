0f5750886a5d83aa97afdb74b7cd484d3380a3bd
AnWalbroel <walbroel.andreas@gmail.com>
2026-06-12 17:12:16 +0200

    feat: add software version for 2026 airborne campaign

--- scripts/preprocessing/preprocessing_grawac.m
+++ scripts/preprocessing/preprocessing_grawac.m
@@ -16,12 +16,16 @@ else
 end
 
 % add info on software version to dataset#
-if datetimeconv(2024,08,09,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2024,10,10,0,0,0) % VAMPIRE-1
+if  datetimeconv(2024,02,09,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2024,02,20,0,0,0) % HAMAG
+    data.radarsw = '5.65';
+elseif datetimeconv(2024,08,09,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2024,10,10,0,0,0) % VAMPIRE-1
     data.radarsw = '5.65';
 elseif datetimeconv(2025,01,25,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2025,03,10,0,0,0) % IOP4H2O (at Ny-Alesund)
     data.radarsw = '5.65';
 elseif datetimeconv(2025,07,02,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2025,09,01,0,0,0) % VAMPIRE-2
     data.radarsw = '5.65';
+elseif datetimeconv(2026,02,02,0,0,0) <= data.time(1) && data.time(1) < datetimeconv(2026,04,20,0,0,0) % COMPEX
+    data.radarsw = '5.67';
 
 else    
     disp('From function preprocessing_grawac: Radar software version not defined!')
